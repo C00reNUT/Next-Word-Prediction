fa5c99f8d5a4a34781eb14d7779a1c43ad38f0a3
rdgozum <ryanpaul.gozum@gmail.com>
2020-09-12 01:48:45 +0800

    Correct typo in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as readme_file:
 setup(
     name="next_word_prediction",
     version="0.1.0",
-    description="Experiment on the Generative Pretrained Transformer 2 (GPT-2) for Language Modeling task using the PyTorch-Transformers library."
+    description="Experiment on the Generative Pretrained Transformer 2 (GPT-2) for Language Modeling task using the PyTorch-Transformers library.",
     long_description=README,
     url="https://github.com/rdgozum/Next-Word-Prediction",
     author="Ryan Paul Gozum",
@@ -18,14 +18,12 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
    ],
-   keywords=["language model"]
+   keywords=["language model"],
    packages=["next_word_prediction"],
    include_package_data=True,
-   install_requires=["numpy, pytorch-transformers, torch"]
+   install_requires=["numpy", "pytorch-transformers", "torch"],
 )
